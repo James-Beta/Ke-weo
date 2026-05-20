cf37591f76b6846a64f3b73f5b3b09980ec56996
James-Beta <jamesbeta99@outlook.com>
2026-05-20 21:18:38 +0300

    Updated Revenue and Expenditure

--- pages/2_revenue_and_expenditure.py
+++ pages/2_revenue_and_expenditure.py
@@ -1,7 +1,25 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title="Government Revenue & Spending",
     layout="wide")
 
+
+# Render the dataframe with a visual mask over the Period column
+if st.button("Show Data"):
+    st.markdown("### Raw Data (Millions, Ksh)")
+    
+    revenue_df = pd.read_csv("data/Processed/rev.csv")
+    
+    st.dataframe(
+        revenue_df,
+        column_config={
+            "Period": st.column_config.DatetimeColumn(
+                "Reporting Month",
+                format="MMMM YYYY",
+            )
+        },
+        hide_index=True
+    )
